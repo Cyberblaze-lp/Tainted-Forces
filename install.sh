1f46c8edbffaeea6c9ff5ef0f9a2df426570b7a2
roidrole <83431927+roidrole@users.noreply.github.com>
2026-08-09 16:45:24 -0400

    remove install.sh, as well

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-HOOKS_DIR=".githooks"
-
-git config core.hooksPath "$HOOKS_DIR"
-git config merge.ignorelist-driver.driver "sh $(git rev-parse --show-toplevel)/.githooks/merge-driver.sh %A %O %B %P"
-chmod +x "$HOOKS_DIR/pre-commit" "$HOOKS_DIR/post-merge" "$HOOKS_DIR/post-rewrite" "$HOOKS_DIR/merge-driver.sh"
